d36cdbcce1b656b5e39b2548999461f3f275c9b2
weirui@1234 <weirui@xiaowangcloud.cn>
2024-01-03 16:07:12 +0800

    markdown detail modify

--- django_markdown_doc/models.py
+++ django_markdown_doc/models.py
@@ -7,6 +7,7 @@ from markdownx.models import MarkdownxField
 class Document(models.Model):
     title = models.CharField(max_length=100)
     content = MarkdownxField()
+    # yaml_content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- django_markdown_doc/views.py
+++ django_markdown_doc/views.py
@@ -5,24 +5,29 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Document
 from .forms import DocumentForm
-from markdownx.utils import markdownify  # 导入markdownify函数
-import yaml
+import markdown2
 
 def document_list(request):
     documents = Document.objects.all()
     return render(request, 'document_list.html', {'documents': documents})
 
 def document_detail(request, document_id):
+    document = get_object_or_404(Document, pk=document_id)
+    extras = ['fenced-code-blocks']  # Specify any additional features/extras here
+    content_html = markdown2.markdown(document.content, extras=extras)
+    return render(request, 'document_detail.html', {'document': document, 'content_html': content_html})
+
     # document = get_object_or_404(Document, pk=document_id)
     # # return render(request, 'document_detail.html', {'document': document})
     # content_markdown = markdownify(document.content)  # 使用markdownify函数将Markdown文本转换为HTML
     # return render(request, 'document_detail.html',
     #               {'document': document, 'content_markdown': content_markdown})  # 将转换后的HTML内容传递给模板
-    document = get_object_or_404(Document, pk=document_id)
-    content_markdown = markdownify(document.content)  # 将Markdown格式内容转换为HTML
-    yaml_data = yaml.safe_load(document.yaml_content)  # 使用PyYAML库解析YAML格式内容
-    return render(request, 'document_detail.html',
-                  {'document': document, 'content_markdown': content_markdown, 'yaml_data': yaml_data})
+
+    # document = get_object_or_404(Document, pk=document_id)
+    # content_markdown = markdownify(document.content)  # 将Markdown格式内容转换为HTML
+    # yaml_data = yaml.safe_load(document.yaml_content)  # 使用PyYAML库解析YAML格式内容
+    # return render(request, 'document_detail.html',
+    #               {'document': document, 'content_markdown': content_markdown, 'yaml_data': yaml_data})
 
 def document_new(request):
     if request.method == 'POST':
